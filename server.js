0591d708da8c74cc61ce08f2f72374260ddc88b8
demir <demir.ab@protonmail.com>
2025-08-30 14:39:18 +0300

    Enable CORS

--- server.js
+++ server.js
@@ -53,6 +53,13 @@ fs.watch(replays, 'utf-8', function(event, filename) {
 // region setup
 ////////////////////////////////////////////////////////////////////////////////
 
+// CORS (https://enable-cors.org/server_expressjs.html)
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // Requests for files in replay.pokemonshowdown.com
 app.use('/portal', express.static(portal));
 
